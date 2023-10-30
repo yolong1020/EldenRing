476db30b692f7aa747d8e696aff8b7f8618c6a3f
Park Yongjin <87303898+yolong1020@users.noreply.github.com>
2023-10-30 13:49:24 +0900

    Update MeleeAttack_Actor.cpp

--- Source/EldenRing/Private/Items/Weapons/MeleeAttack_Actor.cpp
+++ Source/EldenRing/Private/Items/Weapons/MeleeAttack_Actor.cpp
@@ -19,16 +19,8 @@ AMeleeAttack_Actor::AMeleeAttack_Actor()
 	m_weapon_box = CreateDefaultSubobject<UBoxComponent>(TEXT("Weapon Box"));
 	m_weapon_box->SetupAttachment(RootComponent);
 
-	// Controls what kind of collision is enabled for this body
 	m_weapon_box->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-
-	// Changes all ResponseToChannels container for this PrimitiveComponent. to be NewResponse
-	// 모든 채널에 대한 설정
 	m_weapon_box->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
-
-	// Changes a member of the ResponseToChannels container for this PrimitiveComponent.
-	// 단일 채널에 대한 설정
-	//m_weapon_box->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
 
 	m_boxtrace_start = CreateDefaultSubobject<USceneComponent>(TEXT("Box Trace Start"));
 	m_boxtrace_start->SetupAttachment(RootComponent);
@@ -55,7 +47,7 @@ void AMeleeAttack_Actor::Tick(float DeltaTime)
 
 void AMeleeAttack_Actor::OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (false == IsEnableCollision(OtherActor)) { return; }
+	if (false == IsEnableCollision(OtherActor)) return;
 
 	AActor* owner = GetOwner();
 	CHECK_INVALID(owner)
@@ -64,17 +56,10 @@ void AMeleeAttack_Actor::OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, 
 	if (nullptr != component)
 	{
 		AGameCharacter* defender = Cast<AGameCharacter>(OtherActor);
-		if (nullptr != defender)
+		if (nullptr != defender && component == defender->GetParrySphere())
 		{
-			if (component == defender->GetParrySphere())
-			{
-				AGameCharacter* attacker = Cast<AGameCharacter>(owner);
-				if (nullptr != attacker)
-				{
-					attacker->OnTakeStunn();
-				}
-				
-			}
+			AGameCharacter* attacker = Cast<AGameCharacter>(owner);
+			if (nullptr != attacker) { attacker->OnTakeStunn(); }
 		}
 	}
 
@@ -86,35 +71,29 @@ void AMeleeAttack_Actor::OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, 
 		CHECK_INVALID(defender)
 		CHECK_INVALID(attacker)
 
-		if (defender->GetClass() == attacker->GetClass()) { return; }
+		if (defender->GetClass() == attacker->GetClass()) return;
 
-		if (attacker->IsCurrentActionState(FString("Attacking")) &&
-			defender->IsGuardState())
+		if (attacker->IsCurrentActionState(FString("Attacking")) && defender->IsGuardState())
 		{
 			m_ignore_actors.AddUnique(defender);
 
 			defender->OnAttackDefended(m_attack_weight);
 			attacker->OnAttackBlocked(m_attack_weight);
 
-			if (nullptr != m_weapon_sound) { UGameplayStatics::PlaySoundAtLocation(this, m_weapon_sound, GetActorLocation()); }
+			if (nullptr != m_weapon_sound)  { UGameplayStatics::PlaySoundAtLocation(this, m_weapon_sound, GetActorLocation()); }
 			if (nullptr != m_effect_sparks) { m_effect_sparks->SetActive(true); }
-		}
-		else
-		{
-			UE_LOG(LogTemp, Warning, TEXT("Fail Guard"))
 		}
 	}
 	else
 	{
 		const FVector start = m_boxtrace_start->GetComponentLocation();
-		const FVector end	= m_boxtrace_end->GetComponentLocation();
+		const FVector end   = m_boxtrace_end->GetComponentLocation();
 
 		TArray<AActor*> actor_ignore;
 		actor_ignore.Add(this);
 		actor_ignore.Add(owner);
 
-		for (AActor* actor : m_ignore_actors)
-			actor_ignore.AddUnique(actor);
+		for (AActor* actor : m_ignore_actors) { actor_ignore.AddUnique(actor); }
 
 		FHitResult hit;
 		bool hit_result = UKismetSystemLibrary::BoxTraceSingle(this,
@@ -130,25 +109,21 @@ void AMeleeAttack_Actor::OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, 
 		);
 
 		AActor* target = hit.GetActor();
-		if (nullptr != target)
-		{
-			IHitInterface* interface = Cast<IHitInterface>(target);
-			CHECK_INVALID(interface)
+		if (nullptr == target) return;
+		
+		IHitInterface* interface = Cast<IHitInterface>(target);
+		CHECK_INVALID(interface)
 
-			m_ignore_actors.AddUnique(target);
-			if (false == IsEnableCollision(target)) { return; }
+		m_ignore_actors.AddUnique(target);
+		if (false == IsEnableCollision(target)) return;
 
-			DestructableObjectAction(target->GetActorLocation());
+		DestructableObjectAction(target->GetActorLocation());
 
-			UGameplayStatics::ApplyDamage(target, m_value_damage + FMath::FRandRange(0, m_value_damage), GetInstigator()->GetController(), this, UDamageType::StaticClass());
+		UGameplayStatics::ApplyDamage(target, m_value_damage + FMath::FRandRange(0, m_value_damage), GetInstigator()->GetController(), this, UDamageType::StaticClass());
 
-			interface->Execute_GetHit(target, hit.ImpactPoint, m_attack_weight);
+		interface->Execute_GetHit(target, hit.ImpactPoint, m_attack_weight);
 
-			bool is_character = target->ActorHasTag("Player") || target->ActorHasTag("NPC");
-			if (owner->ActorHasTag("NPC") && is_character)
-			{
-				Cast<ANPC_Character>(owner)->SuccessAttack();
-			}
-		}
+		bool is_character = target->ActorHasTag("Player") || target->ActorHasTag("NPC");
+		if (owner->ActorHasTag("NPC") && is_character) { Cast<ANPC_Character>(owner)->SuccessAttack(); }
 	}
 }
