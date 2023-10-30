b1a438bc975bc80ea9f776ace3de34756f9c908d
Park Yongjin <87303898+yolong1020@users.noreply.github.com>
2023-10-30 11:28:31 +0900

    Update C4311.h

--- Source/EldenRing/Public/Characters/NPC/Enemies/C4311.h
+++ Source/EldenRing/Public/Characters/NPC/Enemies/C4311.h
@@ -19,20 +19,20 @@ public:
 	virtual void  SetWeaponCollision(ECollisionEnabled::Type type) override;
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
 
-	virtual void OnChangeVigilanceState(const EVigilanceState& state);
-	virtual void OnMoveCompleted(const FPathFollowingResult& Result) override;
-	virtual void OnNextAttack() override;
-	virtual void OnEndAttack() override;
-	virtual void OnTurnEnd() override;
-	virtual void OnAttackDefended(const EAttackWeight& attack_weight) override;
-	virtual void OnAttackBlocked(const EAttackWeight& attack_weight) override;
-	virtual void OnTakeStunn();
-	virtual void OnTakeExecutionEnd() override;
-	virtual void OnWatchingSwitch() override;
-	virtual void OnRestingSwitch() override;
-	virtual void OnRestingEnd(const FString& section_name) override;
+	virtual void OnChangeVigilanceState(const EVigilanceState& state) override final;
+	virtual void OnMoveCompleted(const FPathFollowingResult& Result) override final;
+	virtual void OnNextAttack() override final;
+	virtual void OnEndAttack() override final;
+	virtual void OnTurnEnd() override final;
+	virtual void OnAttackDefended(const EAttackWeight& attack_weight) override final;
+	virtual void OnAttackBlocked(const EAttackWeight& attack_weight) override final;
+	virtual void OnTakeStunn() override final;
+	virtual void OnTakeExecutionEnd() override final;
+	virtual void OnWatchingSwitch() override final;
+	virtual void OnRestingSwitch() override final;
+	virtual void OnRestingEnd(const FString& section_name) override final;
 
-	virtual bool InAssmeblyPointAction() override;
+	virtual bool InAssmeblyPointAction() override final;
 
 protected:
 	virtual void BeginPlay() override;
@@ -40,11 +40,11 @@ protected:
 	virtual void HitReact(const EGameDirection& hit_direction, const EAttackWeight& attack_weight) override final;
 	virtual void PawnSeen(APawn* seen_pawn) override;
 	
-	virtual void StartWatching() override;
-	virtual void StartCombat() override;
-	virtual void StartConfront() override;
-	virtual void StartAttack() override;
-	virtual void StartTurn(const EGameDirection& direction) override;
+	virtual void StartWatching() override final;
+	virtual void StartCombat() override final;
+	virtual void StartConfront() override final;
+	virtual void StartAttack() override final;
+	virtual void StartTurn(const EGameDirection& direction) override final;
 
 	virtual void OnCheckMissingTarget() override final;
 	virtual void OnWatchingFinished() override final;
