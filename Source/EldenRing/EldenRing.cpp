f5a11c24ce5f649a4b20c433392f25179a321445
Park Yongjin <87303898+yolong1020@users.noreply.github.com>
2023-10-30 11:45:32 +0900

    Update EldenRing.cpp

--- Source/EldenRing/EldenRing.cpp
+++ Source/EldenRing/EldenRing.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Basic.h"
+#include "EldenRing.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Basic, "Basic" );
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EldenRing, "EldenRing" );
