8d4c7c906cfd5509bd435bcb6deae893a22ed110
Park Yongjin <87303898+yolong1020@users.noreply.github.com>
2023-10-30 13:15:37 +0900

    Update InventoryEquipSlot.cpp

--- Source/EldenRing/Private/HUD/Inventory/InventoryEquipSlot.cpp
+++ Source/EldenRing/Private/HUD/Inventory/InventoryEquipSlot.cpp
@@ -22,15 +22,15 @@ void UInventoryEquipSlot::InitSlotWidget(UInventoryEquip* const widget_equip, FI
 	CHECK_INVALID(widget_equip)
 
 	m_inventory_equip	= widget_equip;
-	ItemOnAdd			= func_add;
+	ItemOnAdd		= func_add;
 	ItemOnAddEmpty		= func_add_empty;
 	ItemOnRemoved		= func_remove;
 
-	m_tile_size			= tile_size;
-	m_slot_type			= equip_type;
+	m_tile_size		= tile_size;
+	m_slot_type		= equip_type;
 
-	FVector2D	resolution			= FVector2D(GSystemResolution.ResX, GSystemResolution.ResY);
-	int32		grid_unit			= int32((resolution.X * 0.5) * (11.6 * 0.01));
+	FVector2D	resolution		= FVector2D(GSystemResolution.ResX, GSystemResolution.ResY);
+	int32		grid_unit		= int32((resolution.X * 0.5) * (11.6 * 0.01));
 	FVector2D	slot_bg_size		= FVector2D(slot_size.X * grid_unit, slot_size.Y * grid_unit);
 	double		slot_size_offset	= slot_bg_size.X * 0.14;
 
@@ -50,12 +50,12 @@ void UInventoryEquipSlot::InitSlotWidget(UInventoryEquip* const widget_equip, FI
 	UMaterialInterface* material = nullptr;
 	switch (equip_type)
 	{
-	case EEquipmentType::EET_Head:	 material = LoadObject<UMaterialInterface>(nullptr, *FString("/Game/EldenRing/Materials/HUD/Equipment/M_Equipment_Slot_Head.M_Equipment_Slot_Head"));		break;
-	case EEquipmentType::EET_Body:	 material = LoadObject<UMaterialInterface>(nullptr, *FString("/Game/EldenRing/Materials/HUD/Equipment/M_Equipment_Slot_Body.M_Equipment_Slot_Body"));		break;
-	case EEquipmentType::EET_Arms:	 material = LoadObject<UMaterialInterface>(nullptr, *FString("/Game/EldenRing/Materials/HUD/Equipment/M_Equipment_Slot_Hands.M_Equipment_Slot_Hands"));		break;
-	case EEquipmentType::EET_Legs:	 material = LoadObject<UMaterialInterface>(nullptr, *FString("/Game/EldenRing/Materials/HUD/Equipment/M_Equipment_Slot_Shoes.M_Equipment_Slot_Shoes"));		break;
-	case EEquipmentType::EET_Weapon: material = LoadObject<UMaterialInterface>(nullptr, *FString("/Game/EldenRing/Materials/HUD/Equipment/M_Equipment_Slot_Weapon.M_Equipment_Slot_Weapon"));	break;
-	default: return;
+		case EEquipmentType::EET_Head:	 material = LoadObject<UMaterialInterface>(nullptr, *FString("/Game/EldenRing/Materials/HUD/Equipment/M_Equipment_Slot_Head.M_Equipment_Slot_Head"));	break;
+		case EEquipmentType::EET_Body:	 material = LoadObject<UMaterialInterface>(nullptr, *FString("/Game/EldenRing/Materials/HUD/Equipment/M_Equipment_Slot_Body.M_Equipment_Slot_Body"));	break;
+		case EEquipmentType::EET_Arms:	 material = LoadObject<UMaterialInterface>(nullptr, *FString("/Game/EldenRing/Materials/HUD/Equipment/M_Equipment_Slot_Hands.M_Equipment_Slot_Hands"));	break;
+		case EEquipmentType::EET_Legs:	 material = LoadObject<UMaterialInterface>(nullptr, *FString("/Game/EldenRing/Materials/HUD/Equipment/M_Equipment_Slot_Shoes.M_Equipment_Slot_Shoes"));	break;
+		case EEquipmentType::EET_Weapon: material = LoadObject<UMaterialInterface>(nullptr, *FString("/Game/EldenRing/Materials/HUD/Equipment/M_Equipment_Slot_Weapon.M_Equipment_Slot_Weapon")); break;
+		default: return;
 	}
 	CHECK_INVALID(material)
 	SlotBGImage->SetBrushFromMaterial(material);
@@ -69,14 +69,14 @@ void UInventoryEquipSlot::EquipItem(UItemObject* const item_object)
 	if (m_equiped_item->IsRotated()) { m_equiped_item->RotateItemObject(); }
 
 	AC0000* player = Cast<AC0000>(GetOwningPlayerPawn());
-	if (nullptr != player) {
-		if (EEquipmentType::EET_Weapon == m_slot_type) {
+	if (nullptr != player) 
+	{
+		if (EEquipmentType::EET_Weapon == m_slot_type) 
+		{
 			bool is_right_hand = FString("LeftWeaponSlot") == this->GetName() ? false : true;
 			player->EquipWeaponFromInventory(m_equiped_item, is_right_hand);
 		}
-		else {
-			player->EquipArmorFromInventory(m_equiped_item);
-		}
+		else { player->EquipArmorFromInventory(m_equiped_item); }
 	}
 
 	SlotCanvasPanel->ClearChildren();
@@ -102,26 +102,20 @@ bool UInventoryEquipSlot::NativeOnDrop(const FGeometry& InGeometry, const FDragD
 
 	UItemObject* item_object = Cast<UItemObject>(InOperation->Payload);
 
-	if (nullptr == item_object		||
-		nullptr == SlotCanvasPanel	||
-		false   == IsEnableEquipment(item_object)) 
-	{ return true; }
+	if (nullptr == item_object	||
+	    nullptr == SlotCanvasPanel	||
+	    false   == IsEnableEquipment(item_object)) return true;
 
-	if (nullptr != m_equiped_item) {
-		if (item_object != m_equiped_item) {
-			SlotCanvasPanel->ClearChildren();
-			ItemOnAddEmpty.Execute(m_equiped_item);
+	if (nullptr != m_equiped_item && item_object != m_equiped_item) 
+	{
+		SlotCanvasPanel->ClearChildren();
+		ItemOnAddEmpty.Execute(m_equiped_item);
 
-			AC0000* player = Cast<AC0000>(GetOwningPlayerPawn());
-			if (nullptr == player) { return true; }
+		AC0000* player = Cast<AC0000>(GetOwningPlayerPawn());
+		if (nullptr == player) return true;
 
-			if (EEquipmentType::EET_Weapon == m_slot_type) {
-				player->UnequipWeapon(m_slot_type, !(FString("LeftWeaponSlot") == this->GetName()));
-			}
-			else {
-				player->UnequipArmor(m_slot_type);
-			}
-		}
+		if (EEquipmentType::EET_Weapon == m_slot_type) { player->UnequipWeapon(m_slot_type, !(FString("LeftWeaponSlot") == this->GetName()));}
+		else 					       { player->UnequipArmor(m_slot_type); }
 	}
 
 	EquipItem(item_object);
@@ -133,24 +127,18 @@ bool UInventoryEquipSlot::NativeOnDragOver(const FGeometry& InGeometry, const FD
 	bool drag_result = Super::NativeOnDragOver(InGeometry, InDragDropEvent, InOperation);
 
 	UItemObject* item_object = Cast<UItemObject>(InOperation->Payload);
-
-	if (nullptr == m_item_widget) { return drag_result; }
+	if (nullptr == m_item_widget) return drag_result;
 
 	if (nullptr != m_equiped_item && item_object == m_equiped_item)
 	{
 		SlotCanvasPanel->ClearChildren();
-
 		m_item_widget->InitItemWidget(ItemOnRemoved, m_equiped_item, m_tile_size);
 
 		AC0000* player = Cast<AC0000>(GetOwningPlayerPawn());
-		if (nullptr == player) { return drag_result; }
+		if (nullptr == player) return drag_result;
 
-		if (EEquipmentType::EET_Weapon == m_slot_type) {
-			player->UnequipWeapon(m_slot_type, !(FString("LeftWeaponSlot") == this->GetName()));
-		}
-		else {
-			player->UnequipArmor(m_slot_type);
-		}
+		if (EEquipmentType::EET_Weapon == m_slot_type) { player->UnequipWeapon(m_slot_type, !(FString("LeftWeaponSlot") == this->GetName())); }
+		else 					       { player->UnequipArmor(m_slot_type); }
 	}
 
 	return true;
@@ -166,7 +154,7 @@ void UInventoryEquipSlot::NativeOnDragEnter(const FGeometry& InGeometry, const F
 	if (nullptr == item_object) { box_color = FLinearColor(1, 0, 0, 0.2); }
 
 	bool is_matching = m_slot_type == item_object->GetItemEquipType();
-	box_color = is_matching ? FLinearColor(0, 1, 0, 0.2) : FLinearColor(1, 0, 0, 0.2);
+	box_color 	 = is_matching ? FLinearColor(0, 1, 0, 0.2) : FLinearColor(1, 0, 0, 0.2);
 
 	SlotBorder->SetBrushColor(box_color);
 }
@@ -186,9 +174,9 @@ bool UInventoryEquipSlot::IsEnableEquipment(UItemObject* const item_object)
 	{
 		if (EDragAndDropHUD::EDAD_Equipment == item_object->GetCurrentHUD())
 		{
-			if (nullptr == m_inventory_equip) { return false; }
+			if (nullptr == m_inventory_equip) return false;
 			UInventoryEquipSlot* slot = m_inventory_equip->GetMatchingItemSlot(item_object->GetItemEquipType(), item_object);
-			if (nullptr == slot) { return false; }
+			if (nullptr == slot) return false;
 
 			slot->EquipItem(item_object);
 			SlotBorder->SetBrushColor(FLinearColor(0, 0, 0, 0.2));
@@ -198,7 +186,6 @@ bool UInventoryEquipSlot::IsEnableEquipment(UItemObject* const item_object)
 		else
 		{
 			ItemOnAdd.Execute(item_object, item_object->GetLastInventoryLocation());
-
 			SlotBorder->SetBrushColor(FLinearColor(0, 0, 0, 0.2));
 
 			return false;
