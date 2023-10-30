bb1d2e98644fb3e9e02bcd0e84e94df4806dbf1a
Park Yongjin <87303898+yolong1020@users.noreply.github.com>
2023-10-30 11:31:20 +0900

    Update AssemblePointObjectTypes.h

--- Source/EldenRing/Public/FieldObject/AssemblyPoint/AssemblePointObjectTypes.h
+++ Source/EldenRing/Public/FieldObject/AssemblyPoint/AssemblePointObjectTypes.h
@@ -3,8 +3,8 @@
 UENUM(BlueprintType)
 enum class EAssemblyPointObjectType : uint8
 {
-	EAPOT_None			UMETA(DisplayName = "None"),
-	EAPOT_Bench			UMETA(DisplayName = "Bench"),
-	EAPOT_Floor			UMETA(DisplayName = "Floor"),
-	EAPOT_Max			UMETA(DisplayName = "Max")
+	EAPOT_None	UMETA(DisplayName = "None"),
+	EAPOT_Bench	UMETA(DisplayName = "Bench"),
+	EAPOT_Floor	UMETA(DisplayName = "Floor"),
+	EAPOT_Max	UMETA(DisplayName = "Max")
 };
