01def7beaf4eecb0e3409df36320cc94538724dc
Park Yongjin <87303898+yolong1020@users.noreply.github.com>
2024-02-14 13:32:28 +0900

    Update BTTask_TrackingTarget.cpp

--- Source/EldenRing/Private/Characters/NPC/Enemies/C3251/Tasks/BTTask_TrackingTarget.cpp
+++ Source/EldenRing/Private/Characters/NPC/Enemies/C3251/Tasks/BTTask_TrackingTarget.cpp
@@ -22,18 +22,5 @@ EBTNodeResult::Type UBTTask_TrackingTarget::ExecuteTask(UBehaviorTreeComponent& 
 
 	npc_interface->TrackingTarget();
 
-	// UNavigationSystemV1* nav_sys = UNavigationSystemV1::GetNavigationSystem(GetWorld());
-	// if (!nav_sys) return EBTNodeResult::Failed;
-	// 
-	// FVector origin = OwnerComp.GetBlackboardComponent()->GetValueAsVector(BBKEY_TARGET);
-	// FNavLocation pos;
-
-
-	// if (nav_sys->GetRandomPointInNavigableRadius(origin, 500, pos))
-	// {
-	// 	OwnerComp.GetBlackboardComponent()->GetValueAsVector(BBKEY_PATROLPOS, pos.Location);
-	// 	return EBTNodeResult::Succeeded;
-	// }
-
 	return EBTNodeResult::Succeeded;
 }
